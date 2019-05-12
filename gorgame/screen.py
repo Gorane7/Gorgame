353a1015b1e54f8ef6b0a458ad2dd58a31403aa1
Gorane7 <kristjankv32@gmail.com>
2019-05-12 11:44:49 +0300

    Windows and subwindows that have a colour

--- gorgame/screen.py
+++ gorgame/screen.py
@@ -1,11 +1,32 @@
 import pygame
+from enum import Enum
+
+#CONSTANTS
+#COLORS
+colours = {
+    "green": (0, 220, 0),
+    "blue": (0, 0, 255),
+    "red": (255, 0, 0),
+    "grey": (127, 127, 127),
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
+    "yellow": (255, 255, 0),
+    "pink": (255, 0, 255),
+    "teal": (0, 255, 255),
+    "dark_blue": (0, 0, 127),
+    "dark_teal": (0, 127, 127),
+    "dark_red": (127, 0, 0),
+    "dark_green": (0, 110, 0),
+    "purple": (127, 0, 127),
+}
 
 class Screen:
-    def __init__(self):
+    def __init__(self, size):
         self.clock = pygame.time.Clock()
         pygame.init()
-        self.display = pygame.display.set_mode((700,500))
+        self.display = pygame.display.set_mode(size)
         pygame.display.set_caption("Game")
+        self.window = Window([0,0], [size[0], size[1]], colours["black"], 0, "main window")
 
     def loop(self):
         self.event_handle()
@@ -13,11 +34,48 @@ class Screen:
         self.clock.tick(30)
 
     def event_handle(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
 
     def draw(self):
-        pass
+        self.display.fill(colours["black"])
+
+        self.window.draw(self.display, Coords([0,0]))
+
+        pygame.display.update()
 
     def quit(self):
         pygame.quit()
         quit()
+
+class Window:
+    def __init__(self, loc, size, colour, height, name):
+        self.loc = Coords([loc[0], loc[1]])
+        self.size = Coords([size[0], size[1]])
+        self.colour = colour
+        self.height = height
+        self.windows = []
+        self.name = name
+
+    def add_window(self, loc, size, colour, height, name):
+        self.windows.append(Window(loc, size, colours[colour], height, name))
+        self.sort_windows()
+
+    def sort_windows(self):
+        self.windows = sorted(self.windows, key = lambda x: x.height)
+
+    def get(self, name):
+        for window in self.windows:
+            if window.name == name:
+                return window
+
+    def draw(self, display, delta):
+        display.fill(self.colour, (self.loc.x + delta.x, self.loc.y + delta.y, self.size.x, self.size.y))
+        for window in self.windows:
+            window.draw(display, Coords([self.loc.x + delta.x, self.loc.y + delta.y]))
+
+class Coords:
+    def __init__(self, loc):
+        self.x = loc[0]
+        self.y = loc[1]
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 from gorgame import screen
 
-game = screen.Screen()
+#CONSTANTS
+size = (700,500)
 
-for i in range(100):
+game = screen.Screen(size)
+
+game.window.add_window([20, 20], [100, 200], "green", 1, "gwin")
+game.window.add_window([50, 50], [300, 30], "blue", 0, "bwin")
+game.window.get("gwin").add_window([10, 10], [5, 5], "red", 1, "rdot")
+
+while True:
     game.loop()
-
-game.quit()
